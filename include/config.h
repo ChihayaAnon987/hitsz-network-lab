2091db7f05510b91499582ea582d759acd60730d
ChihayaAnon987 <2048324682@qq.com>
2025-11-16 23:20:37 +0800

    完成小型 Web 服务器

--- include/config.h
+++ include/config.h
@@ -13,7 +13,7 @@
 #else
 #define NET_IF_IP        \
     {                    \
-        169, 254, 82, 211 \
+        169, 254, 176, 211 \
     }  // 自定义网卡ip地址
 #define NET_IF_MAC                         \
     {                                      \
